243506ec774a3b28817d93f522385346639a3a5c
Jhay-Great <alemonai.jenef@gmail.com>
2024-12-30 03:07:07 +0000

    feat: add route paths to route array

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'notifications',
+        loadComponent: () => import('./features/notification/pages/all-notifications/notifications/notifications.component')
+            .then(n => n.NotificationsComponent),
+        title: 'All notifications'
+    },
+    {
+        path: 'notifications/:id',
+        loadComponent: () => import('./features/notification/pages/notification-message/notification-message/notification-message.component')
+            .then(m => m.NotificationMessageComponent),
+        title: 'Message',
+    }
+];
