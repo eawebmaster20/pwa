88cab5981f7925a9364f27c2789b0a96ac1bc9ed
Jhay-Great <alemonai.jenef@gmail.com>
2024-12-31 18:59:41 +0000

    feat: add dark and light theme implementation

---
+++ src/app/core/services/theme/theme.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private activeTheme: 'light' | 'dark' = 'light';
+
+  private applyTheme(theme: 'light' | 'dark'): void {
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+
+  toggleTheme(): void {
+    console.log('called....')
+    this.activeTheme = this.activeTheme === 'light' ? 'dark' : 'light';
+    this.applyTheme(this.activeTheme);
+    localStorage.setItem('theme', this.activeTheme);
+  }
+  
+}
