9c52b968c3dbe494798172ae65a51b93531e7d5b
Jhay-Great <alemonai.jenef@gmail.com>
2024-12-31 08:02:20 +0000

    refactor: replace static date with dynamic signal date

--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+
+// local imports
 import { NotificationCardComponent } from "../../../notification/components/notification-card/notification-card.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink, NotificationCardComponent],
+  imports: [RouterLink, DatePipe, NotificationCardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  displayDate = signal(new Date());
 
 }
