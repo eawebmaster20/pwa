7d020f4475b8127037861f6c12b8bc33826c28f8
Jhay-Great <alemonai.jenef@gmail.com>
2024-12-31 20:15:49 +0000

    feat: add notification service to handle the notification data and shared logic

--- src/app/features/notification/pages/notification-message/notification-message/notification-message.component.ts
+++ src/app/features/notification/pages/notification-message/notification-message/notification-message.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 // local imports
 import { MessagesComponent } from '../../../components/messages/messages.component';
+import { NotificationService } from '../../../service/notification-service/notification.service';
 
 @Component({
   selector: 'app-notification-message',
@@ -11,6 +12,15 @@ import { MessagesComponent } from '../../../components/messages/messages.compone
   templateUrl: './notification-message.component.html',
   styleUrl: './notification-message.component.scss'
 })
-export class NotificationMessageComponent {
+export class NotificationMessageComponent implements OnInit {
+  notification = this.notificationSerivce.selectedNotification();
+  
+  constructor(
+    private notificationSerivce: NotificationService,
+  ) {}
 
+  ngOnInit(): void {
+    // this.notificationSerivce.selectedNotification();
+    console.log(this.notification);
+  }
 }
