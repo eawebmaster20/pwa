c12603d106b9e509f4d3d67cc3b24a4cf408d518
Jhay-Great <alemonai.jenef@gmail.com>
2024-12-31 19:23:09 +0000

    feat: add non-mobile-device component to control viewport display

---
+++ src/app/shared/components/non-mobile-device/non-mobile-device.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-non-mobile-device',
+  standalone: true,
+  imports: [],
+  templateUrl: './non-mobile-device.component.html',
+  styleUrl: './non-mobile-device.component.scss'
+})
+export class NonMobileDeviceComponent {
+
+}
