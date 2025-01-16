1ab6648b73ebc52b1a85fea8cf88450d71e071fb
eawebmaster20 <eaweb20@gmail.com>
2025-01-16 13:20:09 +0000

    feat: test payload update

--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { DatePipe } from '@angular/common';
 // local imports
 import { NotificationCardComponent } from "../../../notification/components/notification-card/notification-card.component";
 import { NavPanelComponent } from '../../../../shared/components/nav-panel/nav-panel.component';
+import { LocalStorageService } from '../../../../core/services/localstorage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,5 +15,10 @@ import { NavPanelComponent } from '../../../../shared/components/nav-panel/nav-p
 })
 export class DashboardComponent {
   displayDate = signal(new Date());
+
+  constructor(private lsService: LocalStorageService){}
+  getNotificationsList(){
+    return this.lsService.getItem('notifications')?.length
+  }
   
 }
